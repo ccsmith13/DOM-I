e8050649f7615a7d8045f4d9645bb63307b928f0
Christine Smith <christinesm@google.com>
2019-10-30 20:14:01 -0700

    finished MVP

--- js/index.js
+++ js/index.js
@@ -40,3 +40,92 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Circular code snippet image
+let codeSnippetImg = document.getElementById('cta-img').src = "img/header-img.png";
+
+//Dom is Awesome text 
+let mainTitleContent = ["Dom","Is","Awesome"];
+let ctaText = document.querySelector('.cta-text');
+let newH1 = document.createElement("h1");
+let newH12 = document.createElement("h1");
+
+ctaText.prepend(newH1);
+ctaText.prepend(newH12);
+
+
+
+ctaTextH1s = document.querySelectorAll('.cta-text > h1');
+console.log(ctaTextH1s);
+
+for(i=0; i<mainTitleContent.length; i++){
+  ctaTextH1s[i].textContent = mainTitleContent[i];
+}
+
+//Populating the Nav Bar 
+let navItems = ["Services", "Product", "Vision", "Features", "About", "Contact"]
+let navigationAnchors = document.querySelectorAll('nav > a');
+
+for (i=0; i < navItems.length; i++){
+  navigationAnchors[i].textContent = navItems[i];
+}
+
+//Get Started text in button 
+let getStartedButton = document.querySelector('.cta-text > button').textContent = "Get Started";
+
+//Populating Top Section & Bottom Section h4 titles
+let SectionHeaders = ["Features","About","Services","Product","Vision"];
+let SectionH4 = document.querySelectorAll(".text-content > h4");
+
+for (i=0; i < SectionHeaders.length; i++){
+  SectionH4[i].textContent = SectionHeaders[i];
+}
+
+//Populating Section paragraphs
+let sectionPContent = ["Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."];
+let sectionPs = document.querySelectorAll(".text-content > p");
+
+for (i=0; i < sectionPContent.length; i++){
+  sectionPs[i].textContent = sectionPContent[i];
+}
+
+//Populating middle img 
+let middleImg = document.getElementById('middle-img').src = "img/mid-page-accent.jpg";
+
+//Footer selection, alignment
+let footerContainer = document.querySelector('footer');
+footerContainer.style.textAlign = "left";
+
+//Contact header insertion & styling
+let newHeading = document.createElement("h4");
+footerContainer.prepend(newHeading);
+let contactHeader = document.querySelector('footer > h4').textContent = "Contact";
+
+//contact paragraph population
+let footerContent = document.querySelector('footer > p');
+let br = document.createElement("br");
+let newFooterP = document.createElement("p");
+footerContainer.append(newFooterP);
+footerContent.textContent = "123 Way 456 Street";
+newFooterP.textContent = "Somewhere, USA";
+footerContent.style.marginBottom = 0;
+
+
+let newFooterParagraph2 = document.createElement("p");
+footerContainer.append(newFooterParagraph2);
+newFooterParagraph2.textContent = "1 (888) 888-8888";
+newFooterParagraph2.style.marginTop = "2%";
+
+
+let newFooterParagraph3 = document.createElement("p");
+footerContainer.append(newFooterParagraph3);
+newFooterParagraph3.textContent = "sales@greatidea.io";
+newFooterParagraph3.style.marginTop = "2%";
+newFooterParagraph3.style.marginBottom = "4%";
+
+let newFooterParagraph4 = document.createElement("p");
+footerContainer.append(newFooterParagraph4);
+newFooterParagraph4.textContent = "Copyright Great Idea! 2018";
+newFooterParagraph4.style.textAlign = "center";
+newFooterParagraph3.style.marginTop = "2%";
+
