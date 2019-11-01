8bf412243ed09e8c91a4f9a0a545d92c0506c0a0
Christine Smith <christinesm@google.com>
2019-11-01 15:02:01 -0700

    revised entire project and did 1 stretch goal

--- js/index.js
+++ js/index.js
@@ -39,106 +39,116 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
 // Circular code snippet image
-let codeSnippetImg = document.getElementById('cta-img').src = "img/header-img.png";
+let codeSnippetImg = document.getElementById("cta-img");
+codeSnippetImg.setAttribute('src', siteContent["cta"]["img-src"]);
 
 //Dom is Awesome text 
-let mainTitleContent = ["Dom","Is","Awesome"];
-let ctaText = document.querySelector('.cta-text');
-let newH1 = document.createElement("h1");
-let newH12 = document.createElement("h1");
 
-ctaText.prepend(newH1);
-ctaText.prepend(newH12);
+ctaTextH1 = document.querySelector('.cta-text > h1');
+ctaTextH1.textContent = siteContent["cta"]["h1"];
 
+ctaText = document.querySelector('.cta-text');
+ctaText.style.textAlign = "center";
 
 
-ctaTextH1s = document.querySelectorAll('.cta-text > h1');
-
-for(i=0; i<mainTitleContent.length; i++){
-  ctaTextH1s[i].textContent = mainTitleContent[i];
-}
-
-//Populating the Nav Bar 
-let navItems = ["Services", "Product", "Vision", "Features", "About", "Contact"]
+//Nav Bar 
 let navigationAnchors = document.querySelectorAll('nav > a');
-let navigationContainer = document.querySelector('nav');
+let navIndex = 1;
+navigationAnchors.forEach(navitem => {
+  navitem.textContent = siteContent["nav"]["nav-item-"+navIndex];
+  navitem.style.color = "green";
+  navIndex +=1;
+});
 
-//change text color to green - this is not working....?
-navigationContainer.style.color = "green";
+//Adding items to nav using appendChild and prepend
+let navContainer = document.querySelector("nav");
+let prependedNavItem = document.createElement("a");
+navContainer.prepend(prependedNavItem);
+let firstNavAnchor = document.querySelector('nav > a');
+firstNavAnchor.textContent = "New!"
+firstNavAnchor.style.color = "green";
 
-//adding items to nav using appendChild and prepend
-let lastNavItem = document.createElement("a");
-lastNavItem.textContent = "Subscriptions";
-navigationContainer.appendChild(lastNavItem);
+let appendedNavItem = document.createElement("a")
+appendedNavItem.id = "last-nav-item";
+navContainer.appendChild(appendedNavItem);
+let lastNavAnchor = document.querySelector('#last-nav-item');
+lastNavAnchor.textContent = "Another New Item!";
+lastNavAnchor.style.color = "green";
 
-let firstNavItem = document.createElement("a");
-firstNavItem.textContent = "Get Started";
-navigationContainer.prepend(firstNavItem);
-
-
-for (i=0; i < navItems.length; i++){
-  navigationAnchors[i].textContent = navItems[i];
-}
 
 //Get Started text in button 
-let getStartedButton = document.querySelector('.cta-text > button').textContent = "Get Started";
+let getStartedButton = document.querySelector('.cta-text > button')
+getStartedButton.textContent = siteContent["cta"]["button"];
 
-//Populating Top Section & Bottom Section h4 titles
-let SectionHeaders = ["Features","About","Services","Product","Vision"];
-let SectionH4 = document.querySelectorAll(".text-content > h4");
+getStartedButton.addEventListener('mouseenter',()=>{
+  event.target.style.backgroundColor = "pink";
+})
 
-for (i=0; i < SectionHeaders.length; i++){
-  SectionH4[i].textContent = SectionHeaders[i];
+getStartedButton.addEventListener('mouseleave',()=>{
+  event.target.style.backgroundColor = "purple";
+})
+
+//New Button that Updates Content 
+let newButton = document.createElement('button');
+newButton.id = "update-button";
+ctaText.append(newButton);
+let newButtonSelector = document.querySelector("#update-button");
+newButtonSelector.textContent = "Update Content";
+
+
+function updateSite(){
+  let pSelector = document.querySelector('p');
+  pSelector.style.backgroundColor = "hotpink";
+  pSelector.textContent = "Look at this new text!";
 }
 
-//Populating Section paragraphs
-let sectionPContent = ["Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.","Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."];
-let sectionPs = document.querySelectorAll(".text-content > p");
+newButtonSelector.addEventListener('click',updateSite, false);
 
-for (i=0; i < sectionPContent.length; i++){
-  sectionPs[i].textContent = sectionPContent[i];
-}
+//Populating all Middle Section Content
+let middleContent = document.querySelectorAll(".text-content > h4, .middle-img,.text-content > p");
+let middleItemIndex = 0;
+middleContent.forEach(item =>{
+  item.textContent = Object.values(siteContent["main-content"])[middleItemIndex];
+  middleItemIndex += 1;
+});
 
-//Populating middle img 
-let middleImg = document.getElementById('middle-img').src = "img/mid-page-accent.jpg";
+//Correcting Middle Img Population
+let middleImg = document.querySelector('.middle-img');
+middleImg.textContent = "";
+middleImg.src = siteContent["main-content"]["middle-img-src"];
 
 //Footer selection, alignment
-let footerContainer = document.querySelector('footer');
+let footerContainer = document.querySelector('.contact');
 footerContainer.style.textAlign = "left";
 
 //Contact header insertion & styling
 let newHeading = document.createElement("h4");
 footerContainer.prepend(newHeading);
-let contactHeader = document.querySelector('footer > h4').textContent = "Contact";
+let contactHeader = document.querySelector('.contact > h4').textContent = siteContent["contact"]["contact-h4"];
 
 //contact paragraph population
-let footerContent = document.querySelector('footer > p');
-let br = document.createElement("br");
+let footerContent = document.querySelector('.contact > p');
 let newFooterP = document.createElement("p");
-footerContainer.append(newFooterP);
-footerContent.textContent = "123 Way 456 Street";
-newFooterP.textContent = "Somewhere, USA";
-footerContent.style.marginBottom = 0;
+footerContent.textContent = siteContent["contact"]["address"];
+footerContainer.style.width = "150px";
 
 
 let newFooterParagraph2 = document.createElement("p");
 footerContainer.append(newFooterParagraph2);
-newFooterParagraph2.textContent = "1 (888) 888-8888";
-newFooterParagraph2.style.marginTop = "2%";
+newFooterParagraph2.textContent = siteContent["contact"]["phone"];
+newFooterParagraph2.style.marginTop = "15px";
 
 
 let newFooterParagraph3 = document.createElement("p");
 footerContainer.append(newFooterParagraph3);
-newFooterParagraph3.textContent = "sales@greatidea.io";
-newFooterParagraph3.style.marginTop = "2%";
-newFooterParagraph3.style.marginBottom = "4%";
+newFooterParagraph3.textContent = siteContent["contact"]["email"];
+newFooterParagraph3.style.marginTop = "15px";
 
-let newFooterParagraph4 = document.createElement("p");
-footerContainer.append(newFooterParagraph4);
-newFooterParagraph4.textContent = "Copyright Great Idea! 2018";
+let newFooterParagraph4 = document.querySelector('footer > p')
+newFooterParagraph4.textContent = siteContent["footer"]["copyright"];
 newFooterParagraph4.style.textAlign = "center";
-newFooterParagraph3.style.marginTop = "2%";
+newFooterParagraph4.style.justifyContent = "center";
 
